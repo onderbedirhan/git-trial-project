6db92e2c5336aaef6dd082700200b14f4ecce34e
Bedirhan Onder <onderbedirhan00@gmail.com>
2024-08-30 15:08:48 +0300

    added connect and remove ethernet functions

--- feature-ethernet.c
+++ feature-ethernet.c
@@ -4,3 +4,13 @@ void call_ethernet_feature()
 {
     printf("Feature Ethernet!\n");
 }
+
+void connect_ethernet()
+{
+    printf("Ethernet connected");
+}
+
+void remove_ethernet()
+{
+    printf("Ethernete removed");
+}
